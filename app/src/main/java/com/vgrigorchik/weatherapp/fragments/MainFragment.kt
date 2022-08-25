f967d47bb7f1e01211e1fee46424e5b2ec63f708
vgrigorchik <vgrigorchik@gmail.ru>
2022-08-25 22:21:47 +0300

    урок 5: спрашиваем разрешение использовать местоположение конец

--- app/src/main/java/com/vgrigorchik/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/vgrigorchik/weatherapp/fragments/MainFragment.kt
@@ -1,16 +1,21 @@
 package com.vgrigorchik.weatherapp.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.vgrigorchik.weatherapp.R
 import com.vgrigorchik.weatherapp.databinding.ActivityMainBinding
 import com.vgrigorchik.weatherapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +25,26 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION))
+            permissionListener()
+        pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
     companion object {
         @JvmStatic
-        fun newInstance() =
-            MainFragment()
+        fun newInstance() = MainFragment()
     }
 }
