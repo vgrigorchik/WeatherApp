e08966fc2dfa11a66c79a8cec6a62d26be7a2542
vgrigorchik <vgrigorchik@gmail.ru>
2022-08-25 20:57:06 +0300

    урок 3: конец

---
+++ app/src/main/java/com/vgrigorchik/weatherapp/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.vgrigorchik.weatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
